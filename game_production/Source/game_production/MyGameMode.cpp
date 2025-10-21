9063f33651af268450d96ce92d7f9c78921e77ad
DESKTOP-0TPKAPQ\GA2A <h1202415006@hiratagakuen.onmicrosoft.com>
2025-10-21 14:31:18 +0900

    土台

--- game_production/Source/game_production/MyGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyGameMode.h"
-#include "MyCharacter.h"
-#include "UObject/ConstructorHelpers.h"
-
-AMyGameMode::AMyGameMode()
-{
-    // デフォルトのポーン（プレイヤーキャラクター）を設定
-    DefaultPawnClass = AMyCharacter::StaticClass();
-}
--- game_production/Source/game_production/game_production.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "game_production.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, game_production, "game_production" );
